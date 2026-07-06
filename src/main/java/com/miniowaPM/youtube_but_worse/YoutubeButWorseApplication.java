dae58ddcc2124793ffca58b71265b43fdfe9ca2e
MiniowaPM <31001@s.pm.szczecin.pl>
2026-07-06 18:54:21 +0200

    init: Spring Boot init

---
+++ src/main/java/com/miniowaPM/youtube_but_worse/YoutubeButWorseApplication.java
@@ -0,0 +1,13 @@
+package com.miniowaPM.youtube_but_worse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeButWorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeButWorseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miniowaPM/youtube_but_worse/YoutubeButWorseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniowaPM.youtube_but_worse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeButWorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
